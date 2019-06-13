dac259bf289dcf14d49c143c5c2c422a7d12aa50
Viacheslav <Viacheslav@DESKTOP-RD9KSK8>
2019-06-13 12:53:59 +0300

    fromated text

--- src/main/java/application/Cipher.java
+++ src/main/java/application/Cipher.java
@@ -4,34 +4,35 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Cipher {
-	private String loginHash = "", passwordHash = "";
-	
-	public String cipherLogin(String log) throws NoSuchAlgorithmException {
-		MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-		messageDigest.update(log.getBytes());
-		StringBuffer buffer = new StringBuffer();
-		byte[] bytee = messageDigest.digest();
-		for(byte b : bytee) {
-			buffer.append(String.format("%02x", b & 0xFF));
-		}
-		System.gc();
-		loginHash = buffer.toString();
-		buffer.delete(0, buffer.length());
-		return loginHash;
-	}
-	
-	public String cipherPassword(String pass) throws NoSuchAlgorithmException {
-		MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-		messageDigest.update(pass.getBytes());
-		StringBuffer buffer = new StringBuffer();
-		byte[] bytee = messageDigest.digest();
-		for(byte b : bytee) {
-			buffer.append(String.format("%02x", b & 0xFF));
-		}
-		System.gc();
-		passwordHash = buffer.toString();
-		buffer.delete(0, buffer.length());
-		return passwordHash;
-	}
+
+    private String loginHash = "", passwordHash = "";
+
+    public String cipherLogin(String log) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        messageDigest.update(log.getBytes());
+        StringBuffer buffer = new StringBuffer();
+        byte[] bytee = messageDigest.digest();
+        for (byte b : bytee) {
+            buffer.append(String.format("%02x", b & 0xFF));
+        }
+        System.gc();
+        loginHash = buffer.toString();
+        buffer.delete(0, buffer.length());
+        return loginHash;
+    }
+
+    public String cipherPassword(String pass) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        messageDigest.update(pass.getBytes());
+        StringBuffer buffer = new StringBuffer();
+        byte[] bytee = messageDigest.digest();
+        for (byte b : bytee) {
+            buffer.append(String.format("%02x", b & 0xFF));
+        }
+        System.gc();
+        passwordHash = buffer.toString();
+        buffer.delete(0, buffer.length());
+        return passwordHash;
+    }
 
 }
